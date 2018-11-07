9f8f53cd5d6002768ff476f23e32b0479657dcca
seyadava@microsoft.com <seyadava@microsoft.com>
2018-11-06 16:13:46 -0800

    initial commit

---
+++ hybridStorage/hybridstorageaccount.go
@@ -0,0 +1,62 @@
+package hybridStorage
+
+import (
+	"context"
+	"fmt"
+
+	"hybridSample/iam"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/storage/mgmt/storage"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/to"
+)
+
+const (
+	errorPrefix = "Cannot create storage account, reason: %v"
+)
+
+func getStorageAccountsClient(activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID, armEndpoint, subscriptionID string) storage.AccountsClient {
+	token, err := iam.GetResourceManagementTokenHybrid(activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID)
+	if err != nil {
+		log.Fatal(fmt.Sprintf(errorPrefix, fmt.Sprintf("Cannot generate token. Error details: %v.", err)))
+	}
+	storageAccountsClient := storage.NewAccountsClientWithBaseURI(armEndpoint, subscriptionID)
+	storageAccountsClient.Authorizer = autorest.NewBearerAuthorizer(token)
+	return storageAccountsClient
+}
+
+// CreateStorageAccount creates a new storage account.
+func CreateStorageAccount(cntx context.Context, accountName, rgName, location, activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID, armEndpoint, subscriptionID string) (s storage.Account, err error) {
+	storageAccountsClient := getStorageAccountsClient(activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID, armEndpoint, subscriptionID)
+	result, err := storageAccountsClient.CheckNameAvailability(
+		cntx,
+		storage.AccountCheckNameAvailabilityParameters{
+			Name: to.StringPtr(accountName),
+			Type: to.StringPtr("Microsoft.Storage/storageAccounts"),
+		})
+	if err != nil {
+		return s, fmt.Errorf(errorPrefix, err)
+	}
+	if *result.NameAvailable != true {
+		return s, fmt.Errorf(errorPrefix, fmt.Sprintf("storage account name [%v] not available", accountName))
+	}
+	future, err := storageAccountsClient.Create(
+		cntx,
+		rgName,
+		accountName,
+		storage.AccountCreateParameters{
+			Sku: &storage.Sku{
+				Name: storage.StandardLRS},
+			Location:                          to.StringPtr(location),
+			AccountPropertiesCreateParameters: &storage.AccountPropertiesCreateParameters{},
+		})
+	if err != nil {
+		return s, fmt.Errorf(fmt.Sprintf(errorPrefix, err))
+	}
+	err = future.WaitForCompletion(cntx, storageAccountsClient.Client)
+	if err != nil {
+		return s, fmt.Errorf(fmt.Sprintf(errorPrefix, fmt.Sprintf("cannot get the storage account create future response: %v", err)))
+	}
+	return future.Result(storageAccountsClient)
+}
---
+++ hybridcompute/hybridcompute.go
@@ -0,0 +1,127 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package hybridcompute
+
+import (
+	"context"
+	"fmt"
+	"hybridSample/hybridnetwork"
+	"hybridSample/iam"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/2017-03-09/compute/mgmt/compute"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/to"
+)
+
+const (
+	publisher   = "Canonical"
+	offer       = "UbuntuServer"
+	sku         = "16.04-LTS"
+	errorPrefix = "Cannot create VM, reason: %v"
+)
+
+// fakepubkey is used if a key isn't available at the specified path in CreateVM(...)
+var fakepubkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7laRyN4B3YZmVrDEZLZoIuUA72pQ0DpGuZBZWykCofIfCPrFZAJgFvonKGgKJl6FGKIunkZL9Us/mV4ZPkZhBlE7uX83AAf5i9Q8FmKpotzmaxN10/1mcnEE7pFvLoSkwqrQSkrrgSm8zaJ3g91giXSbtqvSIj/vk2f05stYmLfhAwNo3Oh27ugCakCoVeuCrZkvHMaJgcYrIGCuFo6q0Pfk9rsZyriIqEa9AtiUOtViInVYdby7y71wcbl0AbbCZsTSqnSoVxm2tRkOsXV6+8X4SnwcmZbao3H+zfO1GBhQOLxJ4NQbzAa8IJh810rYARNLptgmsd4cYXVOSosTX azureuser"
+
+func getVMClient(activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID, armEndpoint, subscriptionID string) compute.VirtualMachinesClient {
+	token, err := iam.GetResourceManagementTokenHybrid(activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID)
+	if err != nil {
+		log.Fatal(fmt.Sprintf(errorPrefix, fmt.Sprintf("Cannot generate token. Error details: %v.", err)))
+	}
+	vmClient := compute.NewVirtualMachinesClientWithBaseURI(armEndpoint, subscriptionID)
+	vmClient.Authorizer = autorest.NewBearerAuthorizer(token)
+	return vmClient
+}
+
+// CreateVM creates a new virtual machine with the specified name using the specified network interface and storage account.
+// Username, password, and sshPublicKeyPath determine logon credentials.
+func CreateVM(ctx context.Context, vmName, nicName, username, password, storageAccountName, sshPublicKeyPath, rgName, location, activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID, armEndpoint, subscriptionID, storageEndpointSuffix string) (vm compute.VirtualMachine, err error) {
+	cntx := context.Background()
+	nic, _ := hybridnetwork.GetNic(cntx, nicName, activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID, armEndpoint, subscriptionID, rgName)
+
+	var sshKeyData string
+	_, err = os.Stat(sshPublicKeyPath)
+	if err == nil {
+		sshBytes, err := ioutil.ReadFile(sshPublicKeyPath)
+		if err != nil {
+			log.Fatalf(fmt.Sprintf(errorPrefix, fmt.Sprintf("failed to read SSH key data: %v", err)))
+		}
+		sshKeyData = string(sshBytes)
+	} else {
+		sshKeyData = fakepubkey
+	}
+	vhdURItemplate := "https://%s.blob." + storageEndpointSuffix + "/vhds/%s.vhd"
+	vmClient := getVMClient(activeDirectoryEndpoint, tenantID, clientID, clientSecret, activeDirectoryResourceID, armEndpoint, subscriptionID)
+	hardwareProfile := &compute.HardwareProfile{
+		VMSize: compute.StandardA1,
+	}
+	storageProfile := &compute.StorageProfile{
+		ImageReference: &compute.ImageReference{
+			Publisher: to.StringPtr(publisher),
+			Offer:     to.StringPtr(offer),
+			Sku:       to.StringPtr(sku),
+			Version:   to.StringPtr("latest"),
+		},
+		OsDisk: &compute.OSDisk{
+			Name: to.StringPtr("osDisk"),
+			Vhd: &compute.VirtualHardDisk{
+				URI: to.StringPtr(fmt.Sprintf(vhdURItemplate, storageAccountName, vmName)),
+			},
+			CreateOption: compute.FromImage,
+		},
+	}
+	osProfile := &compute.OSProfile{
+		ComputerName:  to.StringPtr(vmName),
+		AdminUsername: to.StringPtr(username),
+		AdminPassword: to.StringPtr(password),
+		LinuxConfiguration: &compute.LinuxConfiguration{
+			SSH: &compute.SSHConfiguration{
+				PublicKeys: &[]compute.SSHPublicKey{
+					{
+						Path:    to.StringPtr(fmt.Sprintf("/home/%s/.ssh/authorized_keys", username)),
+						KeyData: to.StringPtr(sshKeyData),
+					},
+				},
+			},
+		},
+	}
+	networkProfile := &compute.NetworkProfile{
+		NetworkInterfaces: &[]compute.NetworkInterfaceReference{
+			{
+				ID: nic.ID,
+				NetworkInterfaceReferenceProperties: &compute.NetworkInterfaceReferenceProperties{
+					Primary: to.BoolPtr(true),
+				},
+			},
+		},
+	}
+	virtualMachine := compute.VirtualMachine{
+		Location: to.StringPtr(location),
+		VirtualMachineProperties: &compute.VirtualMachineProperties{
+			HardwareProfile: hardwareProfile,
+			StorageProfile:  storageProfile,
+			OsProfile:       osProfile,
+			NetworkProfile:  networkProfile,
+		},
+	}
+	future, err := vmClient.CreateOrUpdate(
+		cntx,
+		rgName,
+		vmName,
+		virtualMachine,
+	)
+	if err != nil {
+		return vm, fmt.Errorf(fmt.Sprintf(errorPrefix, err))
+	}
+	err = future.WaitForCompletion(cntx, vmClient.Client)
+	if err != nil {
+		return vm, fmt.Errorf(fmt.Sprintf(errorPrefix, err))
+	}
+	return future.Result(vmClient)
+}
